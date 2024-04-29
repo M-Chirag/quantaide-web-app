0feb59db857951f01fdaa964c87739936befbf68
Chirag Manghani <43926813+M-Chirag@users.noreply.github.com>
2024-04-29 01:53:13 -0700

    fixes to urls

--- src/app/module1/complete/page.tsx
+++ src/app/module1/complete/page.tsx
@@ -18,14 +18,14 @@ export default function Module1Complete() {
               Did you know?{" "}
             </div>
             <div className="mt-4 max-md:max-w-full">
-              The sequence of questions in a survey can influence respondents answers known as question order effects
+              The sequence of questions in a survey can influence respondents&apos; answers, known as &quot;question order effects.&quot;
             </div>
           </div>
         </div>
       </div>
       <div className="flex flex-col self-center px-5 mt-20 w-full max-w-[1117px] max-md:mt-10 max-md:max-w-full">
         <div className="text-2xl font-bold text-black max-md:max-w-full">
-          Whats next?{" "}
+          What&apos;s next?{" "}
         </div>
         <div className="px-px mt-8 max-md:max-w-full">
           <div className="flex gap-5 max-md:flex-col max-md:gap-0">
