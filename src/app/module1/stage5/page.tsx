649bb7c374ca9ba730a45d2c1ad4f8d726c4e4c5
Chirag Manghani <43926813+M-Chirag@users.noreply.github.com>
2024-05-01 01:40:12 -0700

    module1 complete

---
+++ src/app/module1/stage5/page.tsx
@@ -0,0 +1,137 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Sidebar, SidebarItem } from '../Sidebar';
+import Link from 'next/link';
+import { HomeIcon, SettingsIcon, LayoutDashboard } from 'lucide-react';
+import { Img } from '@builder.io/react';
+
+export default function Stage3() {
+  return (
+    <div className='w-full max-w-[1301px] ml-14 mr-14'>
+      <div className='flex flex-row max-md:flex-col max-md:gap-50'>
+        <div className='flex flex-col bg-white'>
+          <div className='self-center w-full max-w-[1372px] max-md:max-w-full'>
+            <div className='flex gap-5 max-md:flex-col max-md:gap-0'>
+              <div className='flex flex-col w-[69%] max-md:ml-0 max-md:w-full'>
+                <div className='flex flex-col mt-9 text-black max-md:mt-10 max-md:max-w-full'>
+                  <div className='flex gap-5 self-start'>
+                    <Link href='/module1/stage4'>
+                      <button>
+                        <img
+                          loading='lazy'
+                          src='../860d96c9cbbdfa6d1135252de5487b1f79a55f9cf012f94b3462c1601ceaaadc.svg'
+                          className='shrink-0 my-auto w-10 aspect-square'
+                        />
+                      </button>
+                    </Link>
+                    <div className='flex flex-col grow shrink-0 justify-end px-5 basis-0 w-fit'>
+                      <div className='text-xs'>Problem Statement</div>
+                      <div className='mt-1 text-xl font-bold'>
+                        Let’s look at some examples of problem statements:
+                      </div>
+                    </div>
+                  </div>
+                  <div className='flex flex-col pl-16 mt-11 text-base max-md:pl-5 max-md:mt-10 max-md:max-w-full'>
+                    <div className='flex gap-5 max-md:flex-col max-md:gap-0'>
+                      <div className='flex flex-col w-[33%] max-md:ml-0 max-md:w-full'>
+                        <div className='flex flex-col grow justify-center p-4 w-full text-base font-semibold text-black bg-white rounded border border-gray-200 border-solid max-md:mt-2'>
+                          <div className='justify-center'>
+                            Requesting social security numbers on job
+                            applications makes potential applicants hesitant to
+                            apply.
+                          </div>
+                        </div>
+                      </div>
+                      <div className='flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full'>
+                        <div className='flex flex-col grow justify-center p-4 w-full text-base font-semibold text-black bg-white rounded border border-gray-200 border-solid max-md:mt-2'>
+                          <div className='justify-center'>
+                            Students are not crossing the roads carefully in
+                            areas with road construction.
+                          </div>
+                        </div>
+                      </div>
+                      <div className='flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full'>
+                        <div className='grow justify-center p-4 w-full text-base font-semibold text-black bg-white rounded border border-gray-200 border-solid max-md:mt-2'>
+                          This website is not easy for users to navigate.
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className='flex gap-5 mt-7 p-4 bg-white rounded-lg border border-gray-200 border-solid max-md:flex-wrap'>
+                      <div className='flex gap-0 justify-center items-center px-2 py-2 bg-amber-500 rounded-md h-[45px] w-[45px]'>
+                        <img
+                          loading='lazy'
+                          src='https://cdn.builder.io/api/v1/image/assets/TEMP/cc06fddba394dd106c0cce3f0a1a6a49d7afe7ca3f0b08eecf043950d3ca5f7e?apiKey=2a09c3227636445ca6cca45824f0323c&'
+                          className='gap-0 w-full border-0 border-white border-solid aspect-[0.92] fill-white stroke-[0.2px] stroke-white'
+                        />
+                      </div>
+                      <div className='gap-0 my-auto text-base leading-7 text-black max-md:flex-wrap max-md:max-w-full'>
+                        <p className='font-bold'>Music and Mental Health</p>
+                        Rising mental health problems in the United States have
+                        become a significant health concern with various factors
+                        contributing to it.
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className='flex justify-end'>
+                    <Link href={'../module1/stage6'} passHref>
+                      <button className='justify-center self-end px-10 py-2.5 mt-12 text-base font-bold text-white bg-amber-500 rounded-md max-md:px-5 max-md:mt-10'>
+                        Next
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Sidebar moduleNumber={'1'}>
+          <SidebarItem
+            number='1'
+            text='Choose your Interest'
+            active={false}
+            stageNumber={5}
+            href='/module1/stage1'
+          />
+          <SidebarItem
+            number='2'
+            text='What goes into problem statement?'
+            active={false}
+            stageNumber={5}
+            href='/module1/stage2'
+          />
+          <SidebarItem
+            number='3'
+            text='Conducting ‘problem-based’ deductive research'
+            active={false}
+            stageNumber={5}
+            href='/module1/stage3'
+          />
+          <SidebarItem
+            number='4'
+            text='The Three Criteria'
+            active={false}
+            stageNumber={5}
+            href='/module1/stage4'
+          />
+          <SidebarItem
+            number='5'
+            text='Examples of problem statements'
+            active={true}
+            stageNumber={5}
+            href='/module1/stage5'
+          />
+          <SidebarItem
+            number='6'
+            text='Let’s get hands on!'
+            active={false}
+            stageNumber={5}
+            href='/module1/stage6'
+          />
+        </Sidebar>
+      </div>
+    </div>
+  );
+}
--- src/app/module1/stage6/page.tsx
+++ src/app/module1/stage6/page.tsx
@@ -1,17 +1,17 @@
-"use client";   
+'use client';
 import React, { useState, FormEvent } from 'react';
-import { Star, CircleX, CircleCheck  } from 'lucide-react';
-import { useRouter } from "next/navigation";
-import { AICard } from '../AICard'; 
+import { Star, CircleX, CircleCheck } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { AICard } from '../AICard';
 
 import Link from 'next/link';
-import { Sidebar, SidebarItem } from '../Sidebar'; 
+import { Sidebar, SidebarItem } from '../Sidebar';
 import { data } from 'autoprefixer';
 import ApiService, { AssessmentResult } from '@/app/ApiService';
 
 // Define the score mappings outside the function since they do not depend on component scope
 const scoreMappings: { [key: number]: { text: string; color: string } } = {
-  0: { text: 'Let\'s try again', color: 'text-red-500' },
+  0: { text: "Let's try again", color: 'text-red-500' },
   1: { text: 'Needs Improvement', color: 'text-red-500' },
   2: { text: 'Needs Improvement', color: 'text-yellow-500' },
   3: { text: 'Fair', color: 'text-yellow-500' },
@@ -23,7 +23,8 @@ export default function Stage6() {
   const router = useRouter();
   const [body, setBody] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const [assessmentResult, setAssessmentResult] = useState<AssessmentResult | null>(null);
+  const [assessmentResult, setAssessmentResult] =
+    useState<AssessmentResult | null>(null);
   const [cardsVisibility, setCardsVisibility] = useState({
     1: true,
     2: true,
@@ -37,9 +38,9 @@ export default function Stage6() {
       // Simulated fetch response
       const data: AssessmentResult = {
         score: 4,
-        text1: "The response text that you want to display in the first card.",
-        text2: "Another response text for the second card.",
-        text3: "Further response text for the third card.",
+        text1: 'The response text that you want to display in the first card.',
+        text2: 'Another response text for the second card.',
+        text3: 'Further response text for the third card.',
       };
       // const formData = new FormData(event.currentTarget); // entire form data is captured here
       // const data = await ApiService.postFormData('https://us-central1-encoded-antenna-362401.cloudfunctions.net/python-http-function', formData);
@@ -57,152 +58,232 @@ export default function Stage6() {
       const textKey = `text${cardNumber}` as keyof AssessmentResult;
       const text = assessmentResult[textKey];
       if (typeof text === 'string') {
-            setBody(text);
-        } 
+        setBody(text);
+      }
     }
   };
 
   const handleCrossClick = (cardNumber: number) => {
-    setCardsVisibility(prevState => ({ ...prevState, [cardNumber]: false }));
+    setCardsVisibility((prevState) => ({ ...prevState, [cardNumber]: false }));
   };
+
   const score = assessmentResult ? assessmentResult.score : 0;
   const assessment = scoreMappings[score];
-  
 
   if (assessmentResult) {
     return (
-       <div className="w-full max-w-[1301px] mx-14">
-      <div className="flex flex-row max-md:flex-col max-md:gap-50">
-        <div className="flex-grow bg-white p-5">
-          <div className="flex gap-1 justify-between self-start text-black">
-            <Link href="/module1/stage2">
-              <button>
-                <img
-                  loading="lazy"
-                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/860d96c9cbbdfa6d1135252de5487b1f79a55f9cf012f94b3462c1601ceaaadc?apiKey=2a09c3227636445ca6cca45824f0323c"
-                  className="shrink-0 my-auto w-10 aspect-square"
-                />
-              </button>
-            </Link>
+      <div className='w-full max-w-[1301px] mx-14'>
+        <div className='flex flex-row max-md:flex-col max-md:gap-50'>
+          <div className='flex-grow bg-white p-5'>
+            <div className='flex gap-1 justify-between self-start text-black'>
+              <Link href='/module1/stage6'>
+                <button>
+                  <img
+                    loading='lazy'
+                    src='https://cdn.builder.io/api/v1/image/assets/TEMP/860d96c9cbbdfa6d1135252de5487b1f79a55f9cf012f94b3462c1601ceaaadc?apiKey=2a09c3227636445ca6cca45824f0323c'
+                    className='shrink-0 my-auto w-10 aspect-square'
+                  />
+                </button>
+              </Link>
             </div>
             <div className='flex flex-row'>
               <div className='flex flex-col w-2/3'>
-          <div className="flex flex-col justify-end px-5">
-                      <div className="text-xs">Problem Statement</div>
-                      <div className="mt-1 text-xl font-bold">
-                        Let’s get hands on!
-                      </div>
-                    </div>
-            <div className="mt-10 ml-4 text-base text-black">
-              Your Assessment
-            </div>
-            {/* Star logic goes here based on assessment.score*/}
-            <div className="flex gap-1 items-center text-3xl font-bold tracking-wider">
-    <div className={`px-4 py-3.5 rounded-md ${assessment?.color}`}>
-      {assessment?.text}
-    </div>
-    {[...Array(5)].map((_, index) => (
-      <Star key={index} className={`my-auto ${index < score ? assessment?.color : 'text-gray-300'}`} />
-    ))}
-  </div>
-            <div
-              className="flex flex-row justify-left w-4/7 h-1/3 px-3.5 py-3.5 mt-2 text-base rounded-lg border border-solid border-zinc-300 text-black resize-vertical"
-            >{body}</div>
-</div>
-          {/* The 3 AI responses will be displayed here. */}
-              <div className="flex flex-col w-1/3">
-            
-              {Object.entries(cardsVisibility).map(([cardNumber, isVisible]) => (
-        isVisible && (
-                  <AICard
-                     key={cardNumber}
-            cardNumber={cardNumber}
-            text={(assessmentResult as any)[`text${cardNumber}`]}
-            onCheckClick={() => handleCheckClick(Number(cardNumber))}
-            onCrossClick={() => handleCrossClick(Number(cardNumber))}
-          />
-        )
-      ))}
+                <div className='flex flex-col justify-end px-5'>
+                  <div className='text-xs'>Problem Statement</div>
+                  <div className='mt-1 text-xl font-bold'>
+                    Let’s get hands on!
+                  </div>
+                </div>
+                <div className='mt-10 ml-4 text-base text-black'>
+                  Your Assessment
+                </div>
+                {/* Star logic goes here based on assessment.score*/}
+                <div className='flex gap-1 items-center text-3xl font-bold tracking-wider'>
+                  <div
+                    className={`px-4 py-3.5 rounded-md ${assessment?.color}`}
+                  >
+                    {assessment?.text}
+                  </div>
+                  {[...Array(5)].map((_, index) => (
+                    <Star
+                      key={index}
+                      className={`my-auto ${
+                        index < score ? assessment?.color : 'text-gray-300'
+                      }`}
+                    />
+                  ))}
+                </div>
+                <div className='flex flex-row justify-left w-4/7 h-1/3 px-3.5 py-3.5 mt-2 text-base rounded-lg border border-solid border-zinc-300 text-black resize-vertical'>
+                  {body}
+                </div>
               </div>
-              
-        </div>
-            
-            
+              {/* The 3 AI responses will be displayed here. */}
+              <div className='flex flex-col w-1/3'>
+                {Object.entries(cardsVisibility).map(
+                  ([cardNumber, isVisible]) =>
+                    isVisible && (
+                      <AICard
+                        key={cardNumber}
+                        cardNumber={cardNumber}
+                        text={(assessmentResult as any)[`text${cardNumber}`]}
+                        onCheckClick={() =>
+                          handleCheckClick(Number(cardNumber))
+                        }
+                        onCrossClick={() =>
+                          handleCrossClick(Number(cardNumber))
+                        }
+                      />
+                    )
+                )}
+              </div>
+            </div>
+
             {/* Finalize button */}
-            <div className="flex justify-end mt-10">
-              <Link href={"../module1/complete"} passHref><button
-                type="submit"
-                className="px-10 py-2.5 text-base font-bold text-white bg-amber-500 rounded-md disabled:bg-amber-300"
-              >
-                Finalize
-              </button></Link>
+            <div className='flex justify-end mt-10'>
+              <Link href={'../module1/complete'} passHref>
+                <button
+                  type='submit'
+                  className='px-10 py-2.5 text-base font-bold text-white bg-amber-500 rounded-md disabled:bg-amber-300'
+                >
+                  Finalize
+                </button>
+              </Link>
             </div>
           </div>
-          
-        <Sidebar moduleNumber={"1"}>
-          <SidebarItem number="1" text="Choose your Interest" active={false} stageNumber="6" href="/module1/stage1" />
-              <SidebarItem number="2" text="What goes into problem statement?" stageNumber="6" active={false} href="/module1/stage2" />
-              <SidebarItem number="3" text="Conducting ‘problem-based’ deductive research" stageNumber="6" active={false} href="/module1/stage3" />
-              <SidebarItem number="4" text="The Three Criteria" stageNumber="6" active={false} href="/module1/stage4" />
-              <SidebarItem number="5" text="Examples of problem statements" stageNumber="6" active={false} href="/module1/stage5" />
-              <SidebarItem number="6" text="Let’s get hands on!" stageNumber="6" active={true} href="/module1/stage6" />
-        </Sidebar>
+
+          <Sidebar moduleNumber={'1'}>
+            <SidebarItem
+              number='1'
+              text='Choose your Interest'
+              active={false}
+              stageNumber='6'
+              href='/module1/stage1'
+            />
+            <SidebarItem
+              number='2'
+              text='What goes into problem statement?'
+              stageNumber='6'
+              active={false}
+              href='/module1/stage2'
+            />
+            <SidebarItem
+              number='3'
+              text='Conducting ‘problem-based’ deductive research'
+              stageNumber='6'
+              active={false}
+              href='/module1/stage3'
+            />
+            <SidebarItem
+              number='4'
+              text='The Three Criteria'
+              stageNumber='6'
+              active={false}
+              href='/module1/stage4'
+            />
+            <SidebarItem
+              number='5'
+              text='Examples of problem statements'
+              stageNumber='6'
+              active={false}
+              href='/module1/stage5'
+            />
+            <SidebarItem
+              number='6'
+              text='Let’s get hands on!'
+              stageNumber='6'
+              active={true}
+              href='/module1/stage6'
+            />
+          </Sidebar>
+        </div>
       </div>
-    </div>
-  
-);
-}
+    );
+  }
 
   return (
-    <div className="w-full max-w-[1301px] mx-14">
-      <div className="flex flex-row max-md:flex-col max-md:gap-50">
-        <div className="flex-grow bg-white p-5">
-          <div className="flex gap-1 justify-between self-start text-black">
-            <Link href="/module1/stage2">
+    <div className='w-full max-w-[1301px] mx-14'>
+      <div className='flex flex-row max-md:flex-col max-md:gap-50'>
+        <div className='flex-grow bg-white p-5'>
+          <div className='flex gap-1 justify-between self-start text-black'>
+            <Link href='/module1/stage5'>
               <button>
                 <img
-                  loading="lazy"
-                  src="../860d96c9cbbdfa6d1135252de5487b1f79a55f9cf012f94b3462c1601ceaaadc.svg"
-                  className="shrink-0 my-auto w-10 aspect-square"
+                  loading='lazy'
+                  src='../860d96c9cbbdfa6d1135252de5487b1f79a55f9cf012f94b3462c1601ceaaadc.svg'
+                  className='shrink-0 my-auto w-10 aspect-square'
                 />
               </button>
             </Link>
           </div>
-          <div className="flex flex-col justify-end px-5">
-                      <div className="text-xs">Problem Statement</div>
-                      <div className="mt-1 text-xl font-bold">
-                        Let’s get hands on!
-                      </div>
-                    </div>
+          <div className='flex flex-col justify-end px-5'>
+            <div className='text-xs'>Problem Statement</div>
+            <div className='mt-1 text-xl font-bold'>Let’s get hands on!</div>
+          </div>
           <form onSubmit={handleSubmit}>
-            <div className="mt-12 text-base font-bold text-black">
+            <div className='mt-12 text-base font-bold text-black'>
               Write your own problem statement below
             </div>
             <textarea
               value={body}
               onChange={(e) => setBody(e.target.value)}
-              className="justify-center w-full px-3.5 py-3.5 mt-2 text-base rounded-lg border border-solid border-zinc-300 text-black resize-vertical"
-              placeholder="Problem statement goes here..."
+              className='justify-center w-full px-3.5 py-3.5 mt-2 text-base rounded-lg border border-solid border-zinc-300 text-black resize-vertical'
+              placeholder='Problem statement goes here...'
               rows={6}
             />
-            <div className="flex justify-end mt-10">
+            <div className='flex justify-end mt-10'>
               <button
-                type="submit"
+                type='submit'
                 disabled={isLoading}
-                className="px-10 py-2.5 text-base font-bold text-white bg-amber-500 rounded-md disabled:bg-amber-300"
+                className='px-10 py-2.5 text-base font-bold text-white bg-amber-500 rounded-md disabled:bg-amber-300'
               >
                 {isLoading ? 'Submitting...' : 'Done'}
               </button>
             </div>
           </form>
         </div>
-        <Sidebar moduleNumber={"1"}>
-          <SidebarItem number="1" text="Choose your Interest" active={false} stageNumber="6" href="/module1/stage1" />
-              <SidebarItem number="2" text="What goes into problem statement?" stageNumber="6" active={false} href="/module1/stage2" />
-              <SidebarItem number="3" text="Conducting ‘problem-based’ deductive research" stageNumber="6" active={false} href="/module1/stage3" />
-              <SidebarItem number="4" text="The Three Criteria" stageNumber="6" active={false} href="/module1/stage4" />
-              <SidebarItem number="5" text="Examples of problem statements" stageNumber="6" active={false} href="/module1/stage5" />
-              <SidebarItem number="6" text="Let’s get hands on!" stageNumber="6" active={true} href="/module1/stage6" />
+        <Sidebar moduleNumber={'1'}>
+          <SidebarItem
+            number='1'
+            text='Choose your Interest'
+            active={false}
+            stageNumber='6'
+            href='/module1/stage1'
+          />
+          <SidebarItem
+            number='2'
+            text='What goes into problem statement?'
+            stageNumber='6'
+            active={false}
+            href='/module1/stage2'
+          />
+          <SidebarItem
+            number='3'
+            text='Conducting ‘problem-based’ deductive research'
+            stageNumber='6'
+            active={false}
+            href='/module1/stage3'
+          />
+          <SidebarItem
+            number='4'
+            text='The Three Criteria'
+            stageNumber='6'
+            active={false}
+            href='/module1/stage4'
+          />
+          <SidebarItem
+            number='5'
+            text='Examples of problem statements'
+            stageNumber='6'
+            active={false}
+            href='/module1/stage5'
+          />
+          <SidebarItem
+            number='6'
+            text='Let’s get hands on!'
+            stageNumber='6'
+            active={true}
+            href='/module1/stage6'
+          />
         </Sidebar>
       </div>
     </div>
