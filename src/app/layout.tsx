e4774621e23d3f5eb35b10f5ef0a64005eb6fde5
Manasssvi <manasvi@berkeley.edu>
2024-05-02 18:34:05 -0700

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,16 +18,16 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className='h-full w-full'>
           <div className='flex md:flex-col mb-10'>
-            <div className='flex md:flex-col justify-center items-start px-10 py-5 w-full text-4xl text-amber-500 max-md:px-5 max-md:max-w-full'>
+            <div className='flex md:flex-col justify-center items-start px-10 w-full text-4xl text-amber-500 max-md:px-5 max-md:max-w-full'>
               <Link href='/'>
                 <div className='flex gap-0.2'>
                   <img
                     loading='lazy'
-                    src='https://cdn.builder.io/api/v1/image/assets/TEMP/187bd80cf52f75aedeb008493f9b6b39d8a2b52dc04bab43aa927c8b2f6119d1?apiKey=2a09c3227636445ca6cca45824f0323c&'
-                    className='shrink-0 self-start aspect-[0.94] w-[35px] mt-2.5'
+                    src='../Logo_final.png'
+                    className='shrink-0 self-start object-contain w-[120px] h-[42px] mt-2.5'
                     alt='Logo'
                   />
-                  <div className='justify-center py-2 mt-n1'>uantaide</div>
+                  {/* <div className='justify-center py-2 mt-n1'>uantaide</div> */}
                 </div>
               </Link>
             </div>
